a7f3ac48ca7bf299f67bafc84109a46f626dd355
Soksurim <briochefavori@gmail.com>
2021-11-21 17:32:41 +0900

    added 42 blackhhole, exp calculator

---
+++ Javascript/42_exp_calculator/js.js
@@ -0,0 +1,300 @@
+var project = "[[&#x27;42cursus-42run&#x27;, 9450], [&#x27;42cursus-42sh&#x27;, 15750], [&#x27;42cursus-abstract-vm&#x27;, 9450], [&#x27;42cursus-avaj-launcher&#x27;, 4200], [&#x27;42cursus-bomberman&#x27;, 25200], [&#x27;42cursus-boot2root&#x27;, 11500], [&#x27;42cursus-camagru&#x27;, 4200], [&#x27;42cursus-cloud-1&#x27;, 9450], [&#x27;42cursus-computorv1&#x27;, 4200], [&#x27;42cursus-computorv2&#x27;, 9450], [&#x27;42cursus-corewar&#x27;, 17500], [&#x27;42cursus-darkly&#x27;, 6300], [&#x27;42cursus-death&#x27;, 35700], [&#x27;42cursus-doom-nukem&#x27;, 15750], [&#x27;42cursus-dr-quine&#x27;, 2520], [&#x27;42cursus-drivers-and-interrupts&#x27;, 15750], [&#x27;42cursus-dslr&#x27;, 6000], [&#x27;42cursus-durex&#x27;, 15750], [&#x27;42cursus-expert-system&#x27;, 9450], [&#x27;42cursus-famine&#x27;, 9450], [&#x27;42cursus-fdf&#x27;, 1000], [&#x27;42cursus-filesystem&#x27;, 15750], [&#x27;42cursus-fix-me&#x27;, 15750], [&#x27;42cursus-fract-ol&#x27;, 1000], [&#x27;42cursus-ft_ality&#x27;, 4200], [&#x27;42cursus-ft_hangouts&#x27;, 4200], [&#x27;42cursus-ft_linear_regression&#x27;, 4200], [&#x27;42cursus-ft_linux&#x27;, 4200], [&#x27;42cursus-ft_ls&#x27;, 4200], [&#x27;42cursus-ft_nmap&#x27;, 15750], [&#x27;42cursus-ft_ping&#x27;, 4200], [&#x27;42cursus-ft_printf&#x27;, 882], [&#x27;42cursus-ft_script&#x27;, 4200], [&#x27;42cursus-ft_select&#x27;, 4200], [&#x27;42cursus-ft_ssl_des&#x27;, 9450], [&#x27;42cursus-ft_ssl_md5&#x27;, 9450], [&#x27;42cursus-ft_ssl_rsa&#x27;, 15750], [&#x27;42cursus-ft_traceroute&#x27;, 4200], [&#x27;42cursus-ft_turing&#x27;, 9450], [&#x27;42cursus-ft_vox&#x27;, 15750], [&#x27;42cursus-gbmu&#x27;, 31500], [&#x27;42cursus-get_next_line&#x27;, 882], [&#x27;42cursus-gomoku&#x27;, 25200], [&#x27;42cursus-guimp&#x27;, 12600], [&#x27;42cursus-h42n42&#x27;, 9450], [&#x27;42cursus-humangl&#x27;, 4200], [&#x27;42cursus-hypertube&#x27;, 15750], [&#x27;42cursus-in-the-shadows&#x27;, 9450], [&#x27;42cursus-kfs-1&#x27;, 15750], [&#x27;42cursus-kfs-2&#x27;, 15750], [&#x27;42cursus-kfs-3&#x27;, 35700], [&#x27;42cursus-kfs-4&#x27;, 25200], [&#x27;42cursus-kfs-5&#x27;, 35700], [&#x27;42cursus-kfs-6&#x27;, 25200], [&#x27;42cursus-kfs-7&#x27;, 35700], [&#x27;42cursus-kfs-8&#x27;, 15750], [&#x27;42cursus-kfs-9&#x27;, 15750], [&#x27;42cursus-kfs-x&#x27;, 35700], [&#x27;42cursus-krpsim&#x27;, 9450], [&#x27;42cursus-lem-ipc&#x27;, 9450], [&#x27;42cursus-lem_in&#x27;, 9450], [&#x27;42cursus-libft&#x27;, 462], [&#x27;42cursus-little-penguin-1&#x27;, 9450], [&#x27;42cursus-malloc&#x27;, 9450], [&#x27;42cursus-matcha&#x27;, 9450], [&#x27;42cursus-matt-daemon&#x27;, 9450], [&#x27;42cursus-minishell&#x27;, 2814], [&#x27;42cursus-mod1&#x27;, 9450], [&#x27;42cursus-multilayer-perceptron&#x27;, 9450], [&#x27;42cursus-music-room&#x27;, 25200], [&#x27;42cursus-n-puzzle&#x27;, 9450], [&#x27;42cursus-nibbler&#x27;, 9450], [&#x27;42cursus-nm-otool&#x27;, 9450], [&#x27;42cursus-override&#x27;, 35700], [&#x27;42cursus-particle-system&#x27;, 15750], [&#x27;42cursus-pestilence&#x27;, 15750], [&#x27;42cursus-philosophers&#x27;, 3360], [&#x27;42cursus-piscine-ocaml&#x27;, 150], [&#x27;42cursus-piscine-python-django&#x27;, 100], [&#x27;42cursus-piscine-ruby-on-rails&#x27;, 100], [&#x27;42cursus-piscine-swift-ios&#x27;, 100], [&#x27;42cursus-piscine-unity&#x27;, 9450], [&#x27;42cursus-process-and-memory&#x27;, 9450], [&#x27;42cursus-push_swap&#x27;, 1855], [&#x27;42cursus-rainfall&#x27;, 25200], [&#x27;42cursus-red-tetris&#x27;, 15750], [&#x27;42cursus-rt&#x27;, 20750], [&#x27;42cursus-rubik&#x27;, 9450], [&#x27;42cursus-scop&#x27;, 9450], [&#x27;42cursus-shaderpixel&#x27;, 15750], [&#x27;42cursus-snow-crash&#x27;, 9450], [&#x27;42cursus-startup-internship&#x27;, 42000], [&#x27;42cursus-strace&#x27;, 9450], [&#x27;42cursus-swifty-companion&#x27;, 4200], [&#x27;42cursus-swifty-proteins&#x27;, 15750], [&#x27;42cursus-swingy&#x27;, 9450], [&#x27;42cursus-taskmaster&#x27;, 9450], [&#x27;42cursus-total-perspective-vortex&#x27;, 9450], [&#x27;42cursus-userspace_digressions&#x27;, 16800], [&#x27;42cursus-war&#x27;, 35700], [&#x27;42cursus-woody-woodpacker&#x27;, 9450], [&#x27;42cursus-xv&#x27;, 22000], [&#x27;42cursus-zappy&#x27;, 25200], [&#x27;42seoul-web-ot&#x27;, 1000000], [&#x27;42seoul_test&#x27;, 1855], [&#x27;apprentissage-1-an-apprentissage-1-an-1&#x27;, 15000], [&#x27;apprentissage-1-an-apprentissage-1-an-2&#x27;, 20000], [&#x27;apprentissage-1-an-apprentissage-1-an-3&#x27;, 22150], [&#x27;apprentissage-1-an-apprentissage-1-an-4&#x27;, 51966], [&#x27;apprentissage-2-ans-1ere-annee-apprentissage-2-ans-1ere-annee-1&#x27;, 15000], [&#x27;apprentissage-2-ans-1ere-annee-apprentissage-2-ans-1ere-annee-2&#x27;, 20000], [&#x27;apprentissage-2-ans-1ere-annee-apprentissage-2-ans-1ere-annee-3&#x27;, 22150], [&#x27;apprentissage-2-ans-1ere-annee-apprentissage-2-ans-1ere-annee-4&#x27;, 33150], [&#x27;apprentissage-2-ans-2eme-annee-apprentissage-2-ans-2eme-annee-1&#x27;, 15000], [&#x27;apprentissage-2-ans-2eme-annee-apprentissage-2-ans-2eme-annee-2&#x27;, 20000], [&#x27;apprentissage-2-ans-2eme-annee-apprentissage-2-ans-2eme-annee-3&#x27;, 22150], [&#x27;apprentissage-2-ans-2eme-annee-apprentissage-2-ans-2eme-annee-4&#x27;, 33150], [&#x27;bgp-at-doors-of-autonomous-systems-is-simple&#x27;, 22450], [&#x27;born2beroot&#x27;, 577], [&#x27;churn&#x27;, 9450], [&#x27;cinema&#x27;, 4200], [&#x27;city-life&#x27;, 15750], [&#x27;cow-neck-tid&#x27;, 20000], [&#x27;cpp-module-08&#x27;, 9660], [&#x27;cub3d&#x27;, 5775], [&#x27;darkly-web&#x27;, 9450], [&#x27;doom_nukem&#x27;, 15750], [&#x27;electronique&#x27;, 35700], [&#x27;freddie-mercury&#x27;, 12600], [&#x27;ft_containers&#x27;, 10042], [&#x27;ft_irc&#x27;, 21630], [&#x27;ft_malcolm&#x27;, 6000], [&#x27;ft_newton&#x27;, 24360], [&#x27;ft_server&#x27;, 1722], [&#x27;ft_services&#x27;, 10042], [&#x27;ft_ssl_md5_try_rules&#x27;, 225], [&#x27;ft_transcendence&#x27;, 24360], [&#x27;fwa&#x27;, 4200], [&#x27;inception&#x27;, 10042], [&#x27;inception-of-things&#x27;, 25450], [&#x27;internship-i&#x27;, 42000], [&#x27;internship-ii&#x27;, 63000], [&#x27;libasm&#x27;, 966], [&#x27;matrix&#x27;, 7000], [&#x27;messagequeue&#x27;, 9450], [&#x27;microservices&#x27;, 4200], [&#x27;minirt&#x27;, 5775], [&#x27;minitalk&#x27;, 1142], [&#x27;ml-module-04&#x27;, 15000], [&#x27;netpractice&#x27;, 3160], [&#x27;old-cpp-module-08&#x27;, 9660], [&#x27;old-irc&#x27;, 21630], [&#x27;old-philosophers&#x27;, 3360], [&#x27;open-project&#x27;, 31500], [&#x27;part_time-i&#x27;, 42000], [&#x27;part_time-ii&#x27;, 63000], [&#x27;pipex&#x27;, 1142], [&#x27;piscine-java&#x27;, 9450], [&#x27;piscine-java-day-00&#x27;, 100], [&#x27;piscine-java-day-01&#x27;, 100], [&#x27;piscine-java-day-02&#x27;, 100], [&#x27;piscine-java-day-03&#x27;, 100], [&#x27;piscine-java-day-04&#x27;, 100], [&#x27;piscine-java-day-05&#x27;, 100], [&#x27;piscine-java-day-06&#x27;, 100], [&#x27;piscine-java-day-07&#x27;, 100], [&#x27;piscine-java-day-08&#x27;, 100], [&#x27;piscine-java-day-09&#x27;, 100], [&#x27;piscine-java-rush-00&#x27;, 150], [&#x27;piscine-java-rush-01&#x27;, 150], [&#x27;piscine-python-data-science&#x27;, 9450], [&#x27;python-module-04&#x27;, 6000], [&#x27;ready-set-boole&#x27;, 7000], [&#x27;restful&#x27;, 4200], [&#x27;so_long&#x27;, 1000], [&#x27;springboot&#x27;, 9450], [&#x27;tweets&#x27;, 9450], [&#x27;uber&#x27;, 9450], [&#x27;understanding-customer&#x27;, 9450], [&#x27;webserv&#x27;, 21630]]";
+var project_parse = JSON.parse(project.replace(/&#x27;/g, '"'));
+
+$(function () {
+    var availableTags = project_parse.map((n) => { return n[0] });
+    $("#select_project").autocomplete({
+        source: availableTags,
+        autoFocus: true
+    });
+});
+
+
+var original_level = 0.0;
+
+var store_chain_project = {
+    labels: ["Current Level"],
+    data: [original_level]
+};
+
+function set_as_original_level() {
+    document.getElementById("current_level").value = original_level;
+    document.getElementById("select_project").value = "";
+    document.getElementById("project_xp").value = "";
+    document.getElementById("result").innerHTML = original_level;
+    document.getElementById("note").value = "100";
+    document.getElementById("resultBlackholesDays").innerHTML = 0;
+    document.getElementById("flexRadioDefault1").checked = false;
+    document.getElementById("flexRadioDefault2").checked = true;
+    store_chain_project.labels.length = 1;
+    store_chain_project.data.length = 1;
+    scatterChart.update();
+    onchangefunc();
+}
+
+function set_as_current_level() {
+    res = document.getElementById("result").innerHTML;
+    document.getElementById("current_level").value = res;
+    project = "unknow";
+    if (document.getElementById("select_project").value != "") {
+        project = document.getElementById("select_project").value;
+    }
+    note = document.getElementById("note").value;
+    have_bonus_coalition = document.getElementById("flexRadioDefault1").checked;
+    if (have_bonus_coalition) {
+        str_coalition = "Yes";
+    }
+    else {
+        str_coalition = "No";
+    }
+    document.getElementById("select_project").value = "";
+    document.getElementById("project_xp").value = "";
+    document.getElementById("flexRadioDefault1").checked = false;
+    document.getElementById("flexRadioDefault2").checked = true;
+    if (store_chain_project.labels.slice(-1)[0] == "") {
+        store_chain_project.labels.pop();
+        store_chain_project.data.pop();
+    }
+    store_chain_project.labels.push(project.replace("42cursus-", "") + "\nNote : " + note + "\nBonus coa: " + str_coalition);
+    store_chain_project.data.push(res);
+    store_chain_project.data.push(null);
+    store_chain_project.labels.push("");
+    scatterChart.update();
+    onchangefunc();
+}
+
+var lst = [
+
+    { 'x': 0, 'y': 0 },
+    { 'x': 1, 'y': 462 },
+    { 'x': 2, 'y': 2688 },
+    { 'x': 3, 'y': 5885 },
+    { 'x': 4, 'y': 11777 },
+    { 'x': 5, 'y': 29217 },
+    { 'x': 6, 'y': 46255 },
+    { 'x': 7, 'y': 63559 },
+    { 'x': 8, 'y': 74340 },
+    { 'x': 9, 'y': 85483 },
+    { 'x': 10, 'y': 95000 },
+    { 'x': 11, 'y': 105630 },
+    { 'x': 12, 'y': 124446 },
+    { 'x': 13, 'y': 145782 },
+    { 'x': 14, 'y': 169932 },
+    { 'x': 15, 'y': 197316 },
+    { 'x': 16, 'y': 228354 },
+    { 'x': 17, 'y': 263508 },
+    { 'x': 18, 'y': 303366 },
+    { 'x': 19, 'y': 348516 },
+    { 'x': 20, 'y': 399672 },
+    { 'x': 21, 'y': 457632 },
+    { 'x': 22, 'y': 523320 },
+    { 'x': 23, 'y': 597786 },
+    // {'x':24, 'y':682164},
+    // {'x':25, 'y':777756},
+    // {'x':26, 'y':886074},
+    // {'x':27, 'y':1008798},
+    // {'x':28, 'y':1147902},
+    // {'x':29, 'y':1305486},
+    // {'x':30, 'y':1484070}
+];
+function onchangefunc() {
+    //Get current_level
+    current_level = document.getElementById("current_level").value;
+    if (!current_level) {
+        return;
+    }
+
+    if (store_chain_project.data.length == 1) {
+        store_chain_project.data.length = 0;
+        store_chain_project.data.push([current_level]);
+        scatterChart.update();
+    }
+
+    //Get project XP
+
+    project_name = document.getElementById("select_project").value;
+    project_xp = 0;
+    for (i = 0; i < project_parse.length; i++) {
+        if (project_parse[i][0] == project_name) {
+            project_xp = project_parse[i][1];
+        }
+    }
+    if (!project_xp) {
+        project_xp = document.getElementById("project_xp").value;
+        if (!project_xp) {
+            return;
+        }
+    } else {
+        document.getElementById("project_xp").value = project_xp;
+    }
+    //Get note
+    note = document.getElementById("note").value;
+    if (!note) {
+        return;
+    }
+    //check if coalition is first
+    iscoalitionfirst = document.getElementById("flexRadioDefault1").checked;
+
+    //Calucate XP
+
+
+    // Get current_xp
+    level_down = Math.floor(current_level);
+    level_down_xp = lst[level_down].y;
+
+    level_sup = Math.ceil(current_level);
+    level_sup_xp = lst[level_sup].y;
+
+    level_xp_total_current = level_sup_xp - level_down_xp;
+    current_xp = level_down_xp + (level_xp_total_current * (current_level - Math.floor(current_level)));
+
+    // Get project_xp_total
+    project_xp = parseInt(project_xp);
+    project_xp_total = project_xp * (parseInt(note) / 100);
+    bonus_xp = 0;
+    if (iscoalitionfirst) {
+        project_xp_total = project_xp_total + (project_xp_total * 0.042);
+    }
+
+    //Calculate Final Xp.
+    final_xp = current_xp + project_xp_total;
+
+    // Calculate Final level
+    var i = 0;
+    for (; i < lst.length; i++) {
+        if (lst[i].y > final_xp) {
+            break;
+        }
+    }
+    min_xp = lst[i - 1].y;
+    max_xp = lst[i].y;
+    tmp_max_xp = max_xp - min_xp;
+    tmp_final_xp = final_xp - min_xp;
+    var final_level = parseInt(lst[i - 1].x) + (tmp_final_xp / tmp_max_xp);
+    final_level = final_level.toFixed(2);
+
+    // Calculate Blackholes Days
+    if (current_level < 7.00) {
+        var blackholesDays = 0;
+        var xp_level_6 = 49980;
+        var xp_level_7 = 63559;
+        if (final_xp > xp_level_7) {
+            final_xp = xp_level_7;
+        }
+        blackholesDays = (Math.pow((final_xp / xp_level_6), 0.45) - (Math.pow((current_xp / xp_level_6), 0.45))) * 483;
+
+        document.getElementById("resultBlackholesDays").innerHTML = blackholesDays.toFixed(2);
+        document.getElementById("resultBlackholesDays").style.color = "rgb(36,36,36)";
+        document.getElementById("TitleresultBlackholesDays").style.color = "rgb(36,36,36)";
+    }
+    else {
+        document.getElementById("resultBlackholesDays").innerHTML = 0;
+        document.getElementById("resultBlackholesDays").style.color = "gray";
+        document.getElementById("TitleresultBlackholesDays").style.color = "gray";
+
+    }
+    document.getElementById("result").innerHTML = final_level;
+    if (final_level) {
+        scatterChart.update();
+    }
+}
+
+Chart.plugins.register({
+    beforeRender: function (chart) {
+        if (chart.config.options.showAllTooltips) {
+            // create an array of tooltips, 
+            // we can't use the chart tooltip because there is only one tooltip per chart
+            chart.pluginTooltips = [];
+            chart.config.data.datasets.forEach(function (dataset, i) {
+                chart.getDatasetMeta(i).data.forEach(function (sector, j) {
+                    chart.pluginTooltips.push(new Chart.Tooltip({
+                        _chart: chart.chart,
+                        _chartInstance: chart,
+                        _data: chart.data,
+                        _options: chart.options.tooltips,
+                        _active: [sector]
+                    }, chart));
+                });
+            });
+            chart.options.tooltips.enabled = false; // turn off normal tooltips
+        }
+    },
+    afterDraw: function (chart, easing) {
+        if (chart.config.options.showAllTooltips) {
+            if (!chart.allTooltipsOnce) {
+                if (easing !== 1) {
+                    return;
+                }
+                chart.allTooltipsOnce = true;
+            }
+            chart.options.tooltips.enabled = true;
+            Chart.helpers.each(chart.pluginTooltips, function (tooltip) {
+                tooltip.initialize();
+                tooltip.update();
+                tooltip.pivot();
+                tooltip.transition(easing).draw();
+            });
+            chart.options.tooltips.enabled = false;
+        }
+    }
+});
+
+var data = {
+    labels: store_chain_project.labels,
+    datasets: [
+        {
+            data: store_chain_project.data,
+            fill: false,
+            borderColor: 'rgb(0,191,255)',
+            tension: 0.1
+        }],
+
+}
+
+var options = {
+    responsive: true,
+    title: {
+        display: true,
+        text: 'XP Planer'
+    },
+    legend: {
+        display: false
+    },
+    showAllTooltips: true,
+    hover: {
+        mode: 'nearest',
+        intersect: true
+    },
+    tooltips: {
+        yAlign: 'bottom',
+        displayColors: false,
+    },
+    scales: {
+        yAxes: [{
+            ticks: {
+                suggestedMin: 10,
+                suggestedMax: 22,
+                stepSize: 1,
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Levels'
+            }
+
+        }],
+        xAxes: [{
+            ticks: {
+                display: false
+            }
+        }]
+    }
+};
+
+var ctx = document.getElementById('xp_lvl');
+
+var scatterChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options,
+}
+);
